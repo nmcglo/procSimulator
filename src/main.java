eea6c85903c22c4985791e1fb4a978cd9e5f3c96
Neil McGlohon <nmcglohon@gmail.com>
2014-10-19 18:42:08 -0400

    Added Main Method

---
+++ src/main.java
@@ -0,0 +1,23 @@
+
+public class main {
+	
+	static int numProcs = 12;
+	static int numCPUs = 4;
+	static int contextSwitchTime = 3;
+	static int RRTimeSlice = 10;
+	static int cpuBoundBurstNum = 8;
+	
+	
+	
+	public static void main(String args[])
+	{
+		
+
+		
+		
+		
+		
+	}
+	
+	
+}
