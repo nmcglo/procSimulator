c1a6d7fef852a92ce8c1a71dfbdee8b333476742
Neil McGlohon <nmcglohon@gmail.com>
2014-10-19 20:22:44 -0400

    Revert 1ce706f..beef823
    This rolls back to commit 1ce706ffca30042a414876d13c88231158b7f26c.

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -196,7 +196,7 @@ public class Scheduler
 			allProcesses.forEach(p -> p.tick());
 			cpus.forEach(cpu -> cpu.tick());
 			cpus.forEach(cpu->{
-				if(cpu.isIdle() && 
+				if(cpu.getProcess() != null && 
 				(cpu.getProcess().getCurrentState() == ProcessState.IOWait ||
 				cpu.getProcess().getCurrentState() == ProcessState.userWait ||
 				cpu.getProcess().getCurrentState() == ProcessState.terminated)){
@@ -221,7 +221,7 @@ public class Scheduler
 			}
 			if(!(readyQueue.isEmpty())){
 				cpus.forEach(cpu ->{
-					if(cpu.isIdle()){
+					if(cpu.getProcess() == null){
 						cpu.addProcess(readyQueue.poll().switchContext(ProcessState.active));
 					}
 				});
@@ -248,7 +248,7 @@ public class Scheduler
 			allProcesses.forEach(p -> p.tick());
 			cpus.forEach(cpu -> cpu.tick());
 			cpus.forEach(cpu->{
-				if(cpu.isIdle() && 
+				if(cpu.getProcess() != null && 
 				(cpu.getProcess().getCurrentState() == ProcessState.IOWait ||
 				cpu.getProcess().getCurrentState() == ProcessState.userWait ||
 				cpu.getProcess().getCurrentState() == ProcessState.terminated)){
@@ -273,7 +273,7 @@ public class Scheduler
 			}
 			if(!(readyQueue.isEmpty())){
 				cpus.forEach(cpu ->{
-					if(cpu.isIdle()){
+					if(cpu.getProcess() == null){
 						cpu.addProcess(readyQueue.poll().switchContext(ProcessState.active));
 					}
 					else if (cpu.getProcess().remCurrentCPUTime() > readyQueue.peek().remCurrentCPUTime()){
